81f40b8b1e25f1ceb2bc7ba8bdb6bd0e87faf803
jmillana <107077959+jmillana@users.noreply.github.com>
2022-06-09 22:06:28 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,21 @@
+import os
 import setuptools
 import pyteams_msg
 
+here = os.path.abspath(os.path.dirname(__file__))
+
+about = {}
+with open(os.path.join(here, "pyteams_msg", "__about__.py"), encoding="utf-8") as f: 
+    exec(f.read(), about)
+    
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name=__project__,
-    version=__version__,
-    author=__author__,
-    author_email=__author_email__,
+    name="pyteams_msg",
+    version=about["__version__"],
+    author=about["__author__"],
+    author_email=about["__author_email__],
     description="Build and post messages to Microsoft Teams",
     long_description=long_description,
     long_description_content_type="text/markdown",
