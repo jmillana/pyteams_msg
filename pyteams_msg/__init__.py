14496826bf3c4b0108cb12457d4cd9d3e979ee5a
jmillana <107077959+jmillana@users.noreply.github.com>
2022-06-09 15:12:17 +0200

    Update __init__.py

--- pyteams_msg/__init__.py
+++ pyteams_msg/__init__.py
@@ -1,5 +1,4 @@
-__project__ = "PyTeams-MSG"
+__project__ = "pyteams_msg"
 __version__ = "develop"
-__keywords__ = ["teams", "message", "declarative UI"]
 __author__ = "Jordi Marimon"
 __author_email_ = "jordi@marimon.tech"
